dd585b856e73f9e1f814332319a8cbc60be7e03a
Hyeju Jeong <hyeju518@gmail.com>
2024-11-08 13:06:06 +0900

    design: changed font and CSS styles in Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,36 @@
-import Head from 'next/head';
-import Navbar from '../components/Navbar';
-import "./globals.css"
+import type { Metadata } from "next";
+import { Rubik } from "next/font/google";
+import Head from "next/head";
+import Navbar from "../components/Navbar";
+import "./globals.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+const rubik = Rubik({
+  subsets: ["latin"],
+  weight: ["300", "500", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "MONSTERATECH EDU",
+  description: "몬스테라텍 디지털 트레이닝",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="ko">
       <Head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=8, viewport-fit=cover" />
+        <meta
+          name="viewport"
+          content="width=device-width, height=device-height, initial-scale=1, maximum-scale=8, viewport-fit=cover"
+        />
         <title>MONSTERATECH EDU</title>
         <meta name="description" content="몬스테라텍 디지털 트레이닝" />
       </Head>
-      <body>
+      <body className={`${rubik.className} antialiased`}>
         <Navbar />
         {children}
       </body>
