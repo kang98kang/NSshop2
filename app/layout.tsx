ca53b44201a1b9908326509741f72f7c9bc16350
Hyeju Jeong <hyeju518@gmail.com>
2024-11-21 13:58:05 +0900

    fix: changed hardcoded image url in og:meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
     description: "Unity Bootcamp 홍보 페이지",
     images: [
       {
-        url: "/og-image.png",
+        url: "https://unity.monsteratech.com/og-image.png",
         width: 1200,
         height: 630,
         alt: "Unity Bootcamp",
